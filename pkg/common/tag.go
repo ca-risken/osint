e4e167555c8afbcafa5b6e32399ab0e301b765e7
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2020-10-27 00:56:57 +0900

    fix common

---
+++ pkg/common/tag.go
@@ -0,0 +1,15 @@
+package common
+
+const (
+	// TagOsint osint tag
+	TagOsint = "osint"
+	// TagSubdomainSearch subdomainsearch tag
+	TagSubdomainSearch = "subdomain"
+
+	// Osint Resource Type
+
+	// TagUnknown unknown service tag
+	TagUnknown = "unknown-resource-type"
+	// TagDomain domain tag
+	TagDomain = "domain"
+)
--- pkg/message/message.go
+++ pkg/message/message.go
@@ -4,6 +4,8 @@ package message
 type OsintQueueMessage struct {
 	DataSource           string `json:"data_source"`
 	RelOsintDataSourceID uint32 `json:"rel_osint_data_source_id"`
+	OsintID              uint32 `json:"osint_id"`
+	OsintDataSourceID    uint32 `json:"osint_data_source_id"`
 	ProjectID            uint32 `json:"project_id"`
 	ResourceName         string `json:"resource_name"`
 	ResourceType         string `json:"resoorce_type"`
