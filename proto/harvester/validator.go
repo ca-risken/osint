fd9df5b4c5b2acda54f4a17b7254ee75cf0f1d8a
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2020-11-18 15:56:43 +0900

    fix proto
    add ListOsintRelatedResource

--- proto/harvester/validator.go
+++ proto/harvester/validator.go
@@ -4,6 +4,14 @@ import (
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 )
 
+// Validate ListOsintRelatedResourceRequest
+func (r *ListOsintRelatedResourceRequest) Validate() error {
+	return validation.ValidateStruct(r,
+		validation.Field(&r.ResourceName, validation.Required, validation.Length(0, 200)),
+		validation.Field(&r.ResourceType, validation.Required, validation.Length(0, 50)),
+	)
+}
+
 // Validate InvokeScanRequest
 func (r *InvokeScanRequest) Validate() error {
 	return validation.ValidateStruct(r,
