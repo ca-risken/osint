442978a139674c882efc4a01d42c868eb13c86bb
iiiidaaa <oeriver@gmail.com>
2021-06-01 18:16:24 +0900

    add certificate expiration tag

--- pkg/common/tag.go
+++ pkg/common/tag.go
@@ -7,6 +7,8 @@ const (
 	TagPrivateExpose = "private-expose"
 	// TagTakeover takeover tag
 	TagTakeover = "takeover"
+	// TagCertificateExpiration certificate-expiration tag
+	TagCertificateExpiration = "certificate-expiration"
 	// Osint Resource Type
 
 	// TagUnknown unknown service tag
