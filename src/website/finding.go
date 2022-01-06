775ba5e3986c79386c09a2c348c7b9b576a0287a
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-01-06 19:50:55 +0900

    add tag

--- src/website/finding.go
+++ src/website/finding.go
@@ -46,6 +46,9 @@ func (s *sqsHandler) putFinding(ctx context.Context, websiteFinding *finding.Fin
 	if err = s.tagFinding(ctx, res.Finding.ProjectId, res.Finding.FindingId, common.TagOsint); err != nil {
 		appLogger.Errorf("Failed to tag finding. tag: %v, error: %v", common.TagOsint, err)
 	}
+	if err = s.tagFinding(ctx, res.Finding.ProjectId, res.Finding.FindingId, common.TagWebsite); err != nil {
+		appLogger.Errorf("Failed to tag finding. tag: %v, error: %v", common.TagWebsite, err)
+	}
 	if err = s.tagFinding(ctx, res.Finding.ProjectId, res.Finding.FindingId, msg.ResourceName); err != nil {
 		appLogger.Errorf("Failed to tag finding. tag: %v, error: %v", msg.ResourceName, err)
 	}
